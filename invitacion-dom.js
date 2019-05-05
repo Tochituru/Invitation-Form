56db465e0c1b8aa8befd67db1df57b047aa22716
Tochituru <tochituru@users.noreply.github.com>
2019-05-05 15:06:13 -0300

    fin de la invitación

--- invitacion-dom.js
+++ invitacion-dom.js
@@ -1,49 +1,53 @@
-var todoElDoc = document.querySelector('body')
-var formulario = document.forms[0];
+const todoElDoc = document.querySelector('body');
+const formulario = document.forms[0];
 
 
 formulario.onsubmit = function validacion(e) {
-    var nombreForm = formulario.elements[0];
-    var nombre = nombreForm.value
-    var edadForm = formulario.elements[1];
-    var edad = edadForm.value;
-    var estadoSoltere = document.getElementById('Soltere');
-    var estadoCasade = document.getElementById('Casade');
-    var estadoPoliamor = document.getElementById('Poliamor');
-    var estadoNoContesta = document.getElementById('no-contesta');
-    var nacionalidadForm = document.querySelector('select');
-    var nacionalidadElegida = nacionalidadForm.selectedIndex
-    var nacionalidad = nacionalidadElegida.value
-    console.log(nacionalidad)
     e.preventDefault();
+    const divNombre = document.getElementsByClassName('name-input');
+    const divEdad = document.getElementsByClassName('age-input');
+    const divEstado = document.getElementsByClassName('estado-checkbox');
+    const nombreForm = formulario.elements[0];
+    const nombre = nombreForm.value
+    const edadForm = formulario.elements[1];
+    const edad = edadForm.value;
+    const estadoSoltere = document.getElementById('Soltere');
+    const estadoCasade = document.getElementById('Casade');
+    const estadoPoliamor = document.getElementById('Poliamor');
+    const estadoNoContesta = document.getElementById('no-contesta');
+    const nacionalidadForm = document.querySelector('select');
+    const indiceElegido = nacionalidadForm.selectedIndex
+    const nacionalidad = nacionalidadForm[indiceElegido].value;
+
+    //validacion
     if (nombre.length === 0) {
-        var nuevoDiv = document.createElement('div');
-        nuevoDiv.textContent = 'Las puertas no se abren a campos vacíos. Completá el nombre';
+        const nuevoDiv = document.createElement('div');
+        nuevoDiv.textContent = 'Las puertas no se abren a campos vacíos. ¡Completá el nombre!';
         nuevoDiv.setAttribute('class', 'error');
         nuevoDiv.setAttribute('id', 'error-de-nombre');
-        todoElDoc.appendChild(nuevoDiv);
+        divNombre[0].appendChild(nuevoDiv);
     };
 
     if (edad.length === 0) {
-        var nuevoDiv = document.createElement('div');
-        nuevoDiv.textContent = 'Las puertas no se abren a campos vacíos. Completá la edad';
+        const nuevoDiv = document.createElement('div');
+        nuevoDiv.textContent = 'Las puertas no se abren a campos vacíos. ¡Completá la edad!';
         nuevoDiv.setAttribute('class', 'error');
         nuevoDiv.setAttribute('id', 'error-de-edad');
-        todoElDoc.appendChild(nuevoDiv);
+        divEdad[0].appendChild(nuevoDiv);
     } else if (edad < 18) {
-        var nuevoDiv = document.createElement('div');
+        const nuevoDiv = document.createElement('div');
         nuevoDiv.textContent = 'No me gustan les críes. ¡Suerte la próxima!';
         nuevoDiv.setAttribute('class', 'error');
         nuevoDiv.setAttribute('id', 'error-de-edad')
-        todoElDoc.appendChild(nuevoDiv);
+        divEdad[0].appendChild(nuevoDiv);
     } else if (edad > 120) {
-        var nuevoDiv = document.createElement('div');
+        const nuevoDiv = document.createElement('div');
         nuevoDiv.textContent = 'I\'m young and I like to be young... ';
         nuevoDiv.setAttribute('class', 'error');
         nuevoDiv.setAttribute('id', 'error-de-edad')
-        todoElDoc.appendChild(nuevoDiv);
+        divEdad[0].appendChild(nuevoDiv);
     };
-    var estado = '';
+    let estado = '';
     if (estadoSoltere.checked) {
         estado = estadoSoltere.value;
     } else if (estadoCasade.checked) {
@@ -55,38 +59,67 @@ formulario.onsubmit = function validacion(e) {
     }
 
     if (!estado) {
-        var nuevoDiv = document.createElement('div');
-        nuevoDiv.textContent = 'Contestá, no seas chote';
+        let nuevoDiv = document.createElement('div');
+        nuevoDiv.textContent = 'Contestá, ¡no seas chote!';
         nuevoDiv.setAttribute('class', 'error');
         nuevoDiv.setAttribute('id', 'error-de-estado-civil')
-        todoElDoc.appendChild(nuevoDiv);
+        divEstado[0].appendChild(nuevoDiv);
     }
-    //cómo hacer para que se borren los mensajes: on change?
 
+    //Borrar errores
+    function BorrarElemento() {
+        this.nextElementSibling.remove()
+    }
+    nombreForm.onfocus = BorrarElemento;
+    edadForm.onfocus = BorrarElemento;
+
+    const errorEstadoCivil = document.getElementById('error-de-estado-civil');
+    estadoSoltere.onclick = function BorrarErrorEstadoCivil() {
+        if (estadoSoltere.checked === true || estadoCasade.checked === true || estadoPoliamor.checked === true || estadoNoContesta.checked) {
+            divEstado[0].removeChild(errorEstadoCivil);
+        }
+    }
+
+    //Crear ficha de invitade
     function FichaDeInvitade(nombre, edad, estado, nacionalidad) {
-        var fichaArray = [nombre, edad, estado, nacionalidad]
-        var categoriaArray = ['Nombre: ', 'Edad: ', 'Estado civil: ', 'Nacionalidad: ']
+        var fichaArray = [nombre, edad, estado, nacionalidad];
+        var categoriaArray = ['Nombre: ', 'Edad: ', 'Estado civil: ', 'Nacionalidad: '];
+        var claseArray = ['ficha-nombre', 'ficha-edad', 'ficha-estado', 'ficha-nacionalidad'];
         divFichaInvitade = document.createElement('div');
         divFichaInvitade.setAttribute('class', 'ficha-de-invitade');
         divFichaInvitade.textContent = 'Tierraplanista';
         todoElDoc.appendChild(divFichaInvitade);
-        function sumarDiv(divFicha) {
-            for (let index = 0; index < fichaArray.length; index++) {
+        function agregarDiv(divFicha) {
+            for (let i = 0; i < fichaArray.length; i++) {
                 var divFicha = document.createElement('div');
-                divFicha.textContent = categoriaArray[index] + fichaArray[index];
+                divFicha.textContent = categoriaArray[i] + fichaArray[i];
+                divFicha.setAttribute('class', claseArray[i]);
+                divFicha.setAttribute('contenteditable', 'true');
                 divFichaInvitade.appendChild(divFicha);
             }
         }
-
-
-        sumarDiv(divFichaInvitade); 
+        agregarDiv(divFichaInvitade);
+        divBotonCerrar = document.createElement('div');
+        divFichaInvitade.appendChild(divBotonCerrar);
+        divBotonCerrar.setAttribute('id', 'close-button');
+        divBotonCerrar.innerHTML = '<i class="far fa-times-circle fa-lg"></i>';
     }
 
     if (nombre.length > 0 && edad > 18 && edad < 120 && estado) {
         FichaDeInvitade(nombre, edad, estado, nacionalidad)
-
     }
-//no funciona la nacionalidad
-//cómo borrar cada invitación
-}
+    //Borrar la ficha
 
+    function BorrarFichaDeInvitade() {
+        divFichaInvitade.remove()        
+    }
+    divBotonCerrar.onclick = BorrarFichaDeInvitade
+
+    //Editar la ficha de invitado
+
+
+
+
+    //PAra editar la ficha, hacer input en vez de div y ccsearlo engaño
+    //Mantengo el div, agarro el .textContent en una variable, creo un input, le pongo el  value de ese .textContext y borro el div. Cuando el usuario hace onblur, pasa de input a div de nuevo.
+}
